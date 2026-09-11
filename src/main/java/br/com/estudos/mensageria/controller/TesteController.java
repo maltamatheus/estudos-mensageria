cc63090b8922b5e24cbae0628bf6dbefc2ce37b2
maltamatheus <maltamatheus@gmail.com>
2026-09-10 21:42:56 -0300

    Teste 01

---
+++ src/main/java/br/com/estudos/mensageria/controller/TesteController.java
@@ -0,0 +1,16 @@
+package br.com.estudos.mensageria.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/testes")
+public class TesteController {
+
+    @GetMapping("/t1")
+    public ResponseEntity<?> teste01(){
+        return ResponseEntity.ok("Teste 01");
+    }
+}
